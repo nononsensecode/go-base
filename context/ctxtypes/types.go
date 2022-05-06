172dfb6a31d1d387220babbc507f9f9aeaa32603
kaushik <kaushikam@gmail.com>
2022-05-06 15:42:19 +0530

    added context types

--- context/ctxtypes/types.go
+++ context/ctxtypes/types.go
@@ -2,6 +2,8 @@ package ctxtypes
 
 type ClientId string
 type Vendor string
+type SecretName string
 
 const CtxClientIdKey ClientId = "clientId"
 const CtxVendorKey Vendor = "vendor"
+const CtxSecretNameKey SecretName = "secretName"
